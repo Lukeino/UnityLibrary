c0b6f075dce45582b2eb9e6e8d30a231a1177a35
Luca <84413484+Luca00IT@users.noreply.github.com>
2023-09-26 21:12:59 +0200

    Create Action.cs

---
+++ 3D/FirstPerson/Action.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionTools
+{
+    /// <summary>
+    /// A class for managing interactable objects in the scene.
+    /// </summary>
+    public class Action
+    {
+        // Collection of strings containing interactable tags
+        private List<string> interactableTags = new List<string>();
+
+        /// <summary>
+        /// Adds interactable tags to the collection.
+        /// </summary>
+        /// <param name="tags">An array of tags to add to the collection.</param>
+        public void AddInteractableTags(string[] tags)
+        {
+            interactableTags.AddRange(tags);
+        }
+
+        /// <summary>
+        /// Checks if the object pointed at is interactable.
+        /// </summary>
+        /// <param name="tagIndex">The index of the tag in the collection if found, otherwise -1.</param>
+        /// <param name="maxRaycastDistance">The maximum distance for the raycast.</param>
+        /// <returns>True if the object is interactable, false otherwise.</returns>
+        public bool IsObjectInteractable(out int tagIndex, float maxRaycastDistance)
+        {
+            tagIndex = -1; // Initialize tag index to -1 (not found)
+
+            // Cast a ray from the camera's position in the direction it's facing
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxRaycastDistance))
+            {
+                // Check if the tag of the hit object is in the collection
+                string hitTag = hit.collider.gameObject.tag;
+                tagIndex = interactableTags.IndexOf(hitTag);
+
+                if (tagIndex != -1)
+                {
+                    // The object is interactable, and its tag is in the collection
+                    return true;
+                }
+            }
+
+            // No interactable object found or the tag is not in the collection
+            return false;
+        }
+    }
+}
