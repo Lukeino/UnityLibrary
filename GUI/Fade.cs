bdde6ce2b7f1ec5fe2b1440745a698935214afc1
Luca <84413484+Luca00IT@users.noreply.github.com>
2023-09-26 20:24:26 +0200

    Update Fade.cs
    CreateRawImage() now creates a RawImage that covers the entire "Canvas" instead of the Camera.

--- GUI/Fade.cs
+++ GUI/Fade.cs
@@ -7,26 +7,38 @@ namespace FadeTools
 {
     public static class Fade
     {
-        // function that creates a black RawImage that covers the entire screen and gradually makes it black
-        public static int CreateRawImage()
+        // function that creates a black RawImage that covers the entire Canvas and gradually makes it black
+        public static void CreateRawImage()
         {
-            // create a new rawimage totally black
+            // Find the Canvas in the scene
+            Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+        
+            if (canvas == null)
+            {
+                Debug.LogError("No Canvas found in the scene. Make sure you have a Canvas GameObject in your scene.");
+                return;
+            }
+        
+            // Create a new GameObject for the RawImage as a child of the Canvas
             GameObject rawImageObject = new GameObject("FadeScreen");
             RawImage rawImage = rawImageObject.AddComponent<RawImage>();
-
-            // set the color of the raw image
+            rawImage.transform.SetParent(canvas.transform, false);
+        
+            // Set the color of the RawImage to black
             rawImage.color = Color.black;
-
-            // cover the entire camera
-            Camera mainCamera = Camera.main;
-            float cameraWidth = mainCamera.orthographicSize * 2 * mainCamera.aspect;
-            float cameraHeight = mainCamera.orthographicSize * 2;
-
-            rawImage.rectTransform.sizeDelta = new Vector2(cameraWidth, cameraHeight);
-            rawImageObject.transform.position = new Vector3(0, 0, 1);
-            rawImageObject.transform.SetParent(mainCamera.transform);
-
-            return 0;
+        
+            // Set the size of the RawImage to match the size of the Canvas
+            RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+            rawImage.rectTransform.sizeDelta = canvasRect.sizeDelta;
+        
+            // Position the RawImage at the same position as the Canvas
+            rawImage.rectTransform.position = canvasRect.position;
+        
+            // Ensure that the RawImage is at a suitable depth
+            rawImageObject.transform.localPosition = Vector3.forward;
+        
+            // You may want to adjust the sorting order of the RawImage if needed
+            rawImageObject.GetComponent<Canvas>().sortingOrder = 1;
         }
 
         // function that makes the screen fade in
