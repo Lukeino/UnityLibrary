d2b0ecb2920c772d31b882aaa450aace5feb149e
Luca <84413484+Luca00IT@users.noreply.github.com>
2023-10-01 11:33:04 +0200

    Update Fade.cs

--- GUI/Fade.cs
+++ GUI/Fade.cs
@@ -11,42 +11,6 @@ namespace FadeTools
     public static class Fade
     {
         /// <summary>
-        /// Creates a black RawImage that covers the entire Canvas and gradually makes it black.
-        /// </summary>
-        public static void CreateRawImage()
-        {
-            // Find the Canvas in the scene
-            Canvas canvas = GameObject.FindObjectOfType<Canvas>();
-
-            if (canvas == null)
-            {
-                Debug.LogError("No Canvas found in the scene. Make sure you have a Canvas GameObject in your scene.");
-                return;
-            }
-
-            // Create a new GameObject for the RawImage as a child of the Canvas
-            GameObject rawImageObject = new GameObject("FadeScreen");
-            RawImage rawImage = rawImageObject.AddComponent<RawImage>();
-            rawImage.transform.SetParent(canvas.transform, false);
-
-            // Set the color of the RawImage to black
-            rawImage.color = Color.black;
-
-            // Set the size of the RawImage to match the size of the Canvas
-            RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-            rawImage.rectTransform.sizeDelta = canvasRect.sizeDelta;
-
-            // Position the RawImage at the same position as the Canvas
-            rawImage.rectTransform.position = canvasRect.position;
-
-            // Ensure that the RawImage is at a suitable depth
-            rawImageObject.transform.localPosition = Vector3.forward;
-
-            // You may want to adjust the sorting order of the RawImage if needed
-            rawImageObject.GetComponent<Canvas>().sortingOrder = 1;
-        }
-
-        /// <summary>
         /// Gradually fades in a RawImage over a specified duration.
         /// </summary>
         /// <param name="rawImage">The RawImage to fade in.</param>
